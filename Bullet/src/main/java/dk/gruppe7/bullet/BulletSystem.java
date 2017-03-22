96b47d6b3f21c192fa4db377ef17c1170f03463b
Harald <officialyellowharold@gmail.com>
2017-03-22 16:52:37 +0100

    Bullet temporary. Different bullet types are not accounted for yet.

--- Bullet/src/main/java/dk/gruppe7/bullet/BulletSystem.java
+++ Bullet/src/main/java/dk/gruppe7/bullet/BulletSystem.java
@@ -11,11 +11,15 @@ import dk.gruppe7.common.IProcess;
 import dk.gruppe7.common.World;
 import dk.gruppe7.common.data.Vector2;
 import dk.gruppe7.shootingcommon.BulletBluePrint;
+import dk.gruppe7.shootingcommon.BulletType;
 import dk.gruppe7.shootingcommon.ShootingData;
 import dk.gruppe7.shootingcommon.ShootingEvent;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 import org.openide.util.lookup.ServiceProvider;
+
 
 /**
  *
@@ -26,20 +30,29 @@ import org.openide.util.lookup.ServiceProvider;
 public class BulletSystem implements IProcess
 {
     List<ShootingEvent> events = ShootingData.getEvents();
-    List<Entity> bullets = new ArrayList<>();
+    List<UUID> bullets = new ArrayList<>();
+    HashMap<UUID, BulletType> bulletTypes = new HashMap<>();
     
     @Override
     public void start(GameData gameData, World world)
     {
-        
+/*
+        //Bullet for testing.
+        BulletBluePrint bluePrint = new BulletBluePrint();
+        bluePrint.setBulletType(BulletType.BULLET);
+        bluePrint.setPosition(new Vector2(55, 55));
+        bluePrint.setVelocity(new Vector2(10, 10));
+        bluePrint.setAccerleration(0.5f);
+        events.add(new ShootingEvent(bluePrint));
+*/
     }
 
     @Override
     public void stop(GameData gameData, World world)
     {
-        for (Entity bullet : bullets)
+        for (UUID bullet : bullets)
         {
-            world.removeEntity(bullet);
+            world.removeEntity(world.getEntityByID(bullet));
         }
     }
 
@@ -52,22 +65,32 @@ public class BulletSystem implements IProcess
             events.remove(0);
         }
         
-        for (Entity bullet : bullets)
+        for (UUID bulletId : bullets)
         {
-            bullet.getPosition().add(1, 1);
+            Entity bullet = world.getEntityByID(bulletId);
+            switch(bulletTypes.get(bullet.getId())){
+                default:
+                    bullet.setPosition(bullet.getPosition().add(bullet.getVelocity().mul(gameData.getDeltaTime())));
+                    bullet.setVelocity(bullet.getVelocity().add(bullet.getAcceleration(), bullet.getAcceleration()));
+                break;
+            }
         }
         
-        System.out.println(bullets.size());
+        if(bullets.size() > 0) System.out.println(bullets.size());
     }
     
     private void makeBullet(BulletBluePrint bluePrint, World world)
     {
         Entity bullet = new Entity();
         bullet.setPosition(bluePrint.getPosition());
-        bullet.setVelocity(new Vector2(10, 10));
+        bullet.setVelocity(bluePrint.getVelocity());
+        bullet.setAcceleration(bluePrint.getAccerleration());
+        bullet.setBounds(bluePrint.getBounds());
+        bullet.setMaxVelocity(bluePrint.getMaxVelocity());
         
         world.addEntity(bullet);
-        bullets.add(bullet);
+        bullets.add(bullet.getId());
+        bulletTypes.put(bullet.getId(), bluePrint.getBulletType());
         
     }
     
