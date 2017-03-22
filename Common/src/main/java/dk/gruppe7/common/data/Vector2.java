e82d3657aa714b7bc87f34f156e2587f1e855ba5
Daniel Holst <moviemastersdk@gmail.com>
2017-03-22 15:50:37 +0100

    Added Vector2 arithmetic methods, The old "add" method as well as the newly added ones no longer affects the object variables, but instead returns a new Vector2 as the result.

--- Common/src/main/java/dk/gruppe7/common/data/Vector2.java
+++ Common/src/main/java/dk/gruppe7/common/data/Vector2.java
@@ -6,13 +6,40 @@ package dk.gruppe7.common.data;
  */
 public class Vector2 {
     public float x,y;
+    
+    public Vector2(){this.x = 0; this.y = 0;}
     public Vector2(float x, float y){this.x = x; this.y = y;}
+   
+    public Vector2 add(float x, float y) {
+        return new Vector2(this.x + x, this.y + y);
+    }
     
+    public Vector2 add(Vector2 other){
+        return new Vector2(this.x + other.x, this.y + other.y);
+    }
     
-    public void add(float x, float y)
-    {
-        this.x += x;
-        this.y += y;
+    public Vector2 sub(float x, float y) {
+        return new Vector2(this.x - x, this.y - y);
+    }
+    
+    public Vector2 sub(Vector2 other){
+        return new Vector2(this.x - other.x, this.y - other.y);
+    }
+    
+    public Vector2 mul(float a) {
+        return new Vector2(this.x * a, this.y * a);
+    }
+    
+    public Vector2 mul(Vector2 other){
+        return new Vector2(this.x * other.x, this.y * other.y);
+    }
+    
+    public Vector2 div(float a) {
+        return new Vector2(this.x / a, this.y / a);
+    }
+    
+    public Vector2 div(Vector2 other){
+        return new Vector2(this.x / other.x, this.y / other.y);
     }
     
     
