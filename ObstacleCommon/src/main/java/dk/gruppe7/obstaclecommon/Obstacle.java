bff394d95c2be1b5c06da54797a10c16bd589023
Mikkel Bjerg <mikkelbjerg95@gmail.com>
2017-04-15 00:02:43 +0200

    Added ObstacleCommon module
    Added ObstacleCommon module to for the time being differentiate between
    entities from the world list better. Could be used in the future for
    handling e.g. moving obstacles.

---
+++ ObstacleCommon/src/main/java/dk/gruppe7/obstaclecommon/Obstacle.java
@@ -0,0 +1,9 @@
+package dk.gruppe7.obstaclecommon;
+
+import dk.gruppe7.common.Entity;
+
+public class Obstacle extends Entity
+{
+    // Currently just to differentiate between entities from the world list.
+    // Could be used for handling different types of obstacles later e.g. moving obstacles.
+}
