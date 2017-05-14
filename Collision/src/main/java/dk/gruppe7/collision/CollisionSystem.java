7a0915df7efc20d899f2cd1689699dffb6673b4d
Mikkel Bjerg <mikkelbjerg95@gmail.com>
2017-05-14 21:29:48 +0200

    Fixed indexoutofbounds caused by the collisionsystem

--- Collision/src/main/java/dk/gruppe7/collision/CollisionSystem.java
+++ Collision/src/main/java/dk/gruppe7/collision/CollisionSystem.java
@@ -11,7 +11,9 @@ import dk.gruppe7.common.data.Entity;
 import dk.gruppe7.common.GameData;
 import dk.gruppe7.common.IProcess;
 import dk.gruppe7.common.World;
+import dk.gruppe7.common.annotations.Event;
 import dk.gruppe7.common.data.Rectangle;
+import java.util.ArrayList;
 import java.util.List;
 import org.openide.util.lookup.ServiceProvider;
 
@@ -35,6 +37,7 @@ public class CollisionSystem implements IProcess {
 
     @Override
     public void process(GameData gameData, World world) {
+        List<Object> listOfEventsToPost = new ArrayList<>();
         List<Entity> list = world.getEntities();
         for(int i = 0; i < list.size(); i++) {
             for(int j = i + 1; j < list.size(); j++) {
@@ -43,11 +46,13 @@ public class CollisionSystem implements IProcess {
                         continue;
                     }
                     
-                    Dispatcher.post(new CollisionEvent(list.get(i).getId(), list.get(j).getId()), world);
-                    Dispatcher.post(new CollisionEvent(list.get(j).getId(), list.get(i).getId()), world);
+                    listOfEventsToPost.add(new CollisionEvent(list.get(i).getId(), list.get(j).getId()));
+                    listOfEventsToPost.add(new CollisionEvent(list.get(j).getId(), list.get(i).getId()));
                 }
             }
         }
+        
+        listOfEventsToPost.forEach(event -> Dispatcher.post(event, world));
     }
     
     private boolean intersects(Entity targetEntity, Entity otherEntity) {
